55f5148cc7b04f7fe946e7afd71bfc1030bd6a40
Tiago S. Hucs <tiagohucs@gmail.com>
2021-12-20 16:22:44 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+document.getElementById("redDiv").onmousedown = function () {
+    console.log("Click vermelho");
+    return true; // Not needed, as long as you don't return false
+};
+
+document.getElementById("yelDiv").onmousedown = function () {
+    console.log("Click amarelo");
+    return true; // Not needed, as long as you don't return false
+};
+
+document.getElementById("greDiv").onmousedown = function () {
+    console.log("Click verde");
+    return true; // Not needed, as long as you don't return false
+};
+
+document.getElementById("bluDiv").onmousedown = function () {
+    console.log("Click azul");
+    return true; // Not needed, as long as you don't return false
+};
